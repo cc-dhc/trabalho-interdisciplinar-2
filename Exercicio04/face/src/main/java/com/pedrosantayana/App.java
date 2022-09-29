55cd4effd73f26f10d9e42905fb49ff5d8f1b187
Pedro Santayana <pedrosantayana@gmail.com>
2022-09-29 19:42:48 -0300

    final

---
+++ Exercicio04/face/src/main/java/com/pedrosantayana/App.java
@@ -0,0 +1,78 @@
+package com.pedrosantayana;
+
+import java.net.URI;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class App {
+	private static final String subscriptionKey = "64202cd2d7ff4899b2c06d470704b108";
+	private static final String endpoint = "https://face-pucmg.cognitiveservices.azure.com/";
+
+    private static final String imageWithFaces =
+        "{\"url\":\"https://upload.wikimedia.org/wikipedia/commons/c/c3/RH_Louise_Lillian_Gish.jpg\"}";
+// </environment>
+
+// <main>
+    public static void main(String[] args) {
+        HttpClient httpclient = HttpClientBuilder.create().build();
+
+        try
+        {
+            URIBuilder builder = new URIBuilder(endpoint + "/face/v1.0/detect");
+
+            // Request parameters. All of them are optional.
+			builder.setParameter("detectionModel", "detection_03");
+            builder.setParameter("returnFaceId", "true");
+
+            // Prepare the URI for the REST API call.
+            URI uri = builder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            // Request body.
+            StringEntity reqEntity = new StringEntity(imageWithFaces);
+            request.setEntity(reqEntity);
+
+            // Execute the REST API call and get the response entity.
+            HttpResponse response = httpclient.execute(request);
+            HttpEntity entity = response.getEntity();
+// </main>
+
+// <print>
+            if (entity != null)
+            {
+                // Format and display the JSON response.
+                System.out.println("REST Response:\n");
+
+                String jsonString = EntityUtils.toString(entity).trim();
+                if (jsonString.charAt(0) == '[') {
+                    JSONArray jsonArray = new JSONArray(jsonString);
+                    System.out.println(jsonArray.toString(2));
+                }
+                else if (jsonString.charAt(0) == '{') {
+                    JSONObject jsonObject = new JSONObject(jsonString);
+                    System.out.println(jsonObject.toString(2));
+                } else {
+                    System.out.println(jsonString);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            // Display error message.
+            System.out.println(e.getMessage());
+        }
+    }
+}
+// </print>
